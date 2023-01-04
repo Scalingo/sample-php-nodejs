c1463aecc7d70d5382580d0073e7ad5f226f54fb
Mederic Bazart <mederic.bazart@scalingo.com>
2023-01-04 18:31:19 +0100

    chore(dependencies): update dependencies and languages versions for stack scalingo-22

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('sass', function () {
-    gulp.src('assets/sass/style.scss')
-      .pipe(sass({
-        includePaths: ['assets/sass'],
-        errLogToConsole: true
-      }))
-      .pipe(gulp.dest('assets/css'));
+gulp.task('sass', function (done) {
+  gulp.src('assets/sass/style.scss')
+    .pipe(sass({
+      includePaths: ['assets/sass'],
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest('assets/css'));
+  done();
 });
-
-gulp.task('bundle', ['sass']);
-gulp.task('default', ['bundle']);
-
+gulp.task('bundle', gulp.series('sass'));
+gulp.task('default', gulp.series('bundle'));
